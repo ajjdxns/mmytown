c16d46b899ae194f32ac9c88b1988eeeb6c12222
ajjdxns <wyj121023@163.com>
2024-01-14 20:27:04 +0800

    new file:   app.js 	new file:   img/title.jpg 	new file:   index.html 	new file:   main.css

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    console.log('[INFO]current class name: ' + className);
+});
